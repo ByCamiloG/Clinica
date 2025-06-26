ad00f1f65894d318aa945ec4f9cf01444fa1e717
juanc <juanc@PC-JUAN-ZAMORA>
2025-06-25 22:15:31 -0500

    Version 1

---
+++ src/main/java/proyecto/Main.java
@@ -0,0 +1,9 @@
+package Proyecto;
+
+import View.LoginFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> new LoginFrame());
+    }
+}
